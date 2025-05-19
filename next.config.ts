cd34ea7e475fb68a49034ead0aebd1c4de05c185
tomada1114 <tmasuyama1114@gmail.com>
2025-05-19 16:12:53 -0600

    basePath設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // サイト2がサブディレクトリとしてアクセスされるため、basePath設定が必要
+  basePath: '/blog-test',
+
+  // 画像などのアセットにもbasePath接頭辞を付ける
+  assetPrefix: '/blog-test',
 };
 
 export default nextConfig;
